f5a5227393d3b7109f02c343d7dbf4ae2b9fce12
thanhnguyenntt <94180709+thanhnguyenntt@users.noreply.github.com>
2022-12-09 21:10:32 +0700

    insert inHoaDon()

--- src/main/java/edu/poly/shopbangiay/view/HoaDonUI.java
+++ src/main/java/edu/poly/shopbangiay/view/HoaDonUI.java
@@ -70,6 +70,7 @@ public class HoaDonUI extends javax.swing.JPanel {
             });
         }
     }
+    public void inHoaDon(){}
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
